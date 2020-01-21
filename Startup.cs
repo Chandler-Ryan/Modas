c6fd779a098e9c3d8880241bd2cde8beb279ffb0
Chandler <chandler.ryan@live.com>
2020-01-20 19:35:04 -0600

    midclass

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Modas.Models;
 
 namespace modas
 {
@@ -15,6 +16,8 @@ namespace modas
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IEventRepository, FakeEventRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +28,8 @@ namespace modas
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
         }
     }
 }
